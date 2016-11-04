8802d416218daf523a97d49118255e479438304e
pointdeveloper <pointdeveloper99@gmail.com>
2016-11-04 22:50:41 +0530

    inital commit

---
+++ pages/about/about.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+  tabBarElement: any;
+  constructor(public navCtrl: NavController) {
+    this.tabBarElement = document.querySelector('.tabbar.show-tabbar');
+  }
+
+  ionViewWillEnter() {
+    this.tabBarElement.style.display = 'none';
+  }
+
+  ionViewWillLeave() {
+    this.tabBarElement.style.display = 'flex';
+  }
+
+  takeMeBack() {
+    this.navCtrl.parent.select(0);
+    console.log(this.navCtrl.parent);
+    
+  }
+
+}
